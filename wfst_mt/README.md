6dd29d11a8f98f4932903af830abea041524180b
ye-kyaw-thu <ykt.nlp.ai@gmail.com>
2025-07-19 10:46:00 +0700

    wfst_mt deleted

--- wfst_mt/README.md
+++
@@ -1,3 +0,0 @@
-# WFST MT
-
-[WFST_MT.ipynb](https://github.com/ye-kyaw-thu/wfst_nlp_tutorials/blob/main/wfst_mt/my-rk/WFST_MT.ipynb)  
--- wfst_mt/my-rk/shortest-path-to-line.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# How to run:
-# e.g. bash ./shortest-path-to-line.sh ./shortest-path.fst
-
-fstprint $1 | tac | cut -f4 | tr '\n' ' ' | awk 'BEGIN {FS=" ";OFS=" "} {print $NF; for(i=1;i<NF-1;++i) print $i;}' | tr '\n' ' '
